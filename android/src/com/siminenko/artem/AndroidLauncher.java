30b29fa8216b24ea2aa7ef52f4cca59d90f837f9
artemka124 <avl123sim@mail.ru>
2018-05-28 17:44:54 +0300

    fix

--- android/src/com/siminenko/artem/AndroidLauncher.java
+++ android/src/com/siminenko/artem/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.android.gms.analytics.GoogleAnalytics;
 import com.siminenko.artem.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
